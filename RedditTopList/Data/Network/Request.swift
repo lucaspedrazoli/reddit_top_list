9ae8e3012a9e63c9402ee50ebe2649b408a51a2a
Lucas <lucaspedrazoli@gmail.com>
2021-06-21 15:47:00 -0300

    create request

---
+++ RedditTopList/Data/Network/Request.swift
@@ -0,0 +1,44 @@
+//
+//  Request.swift
+//  RedditTopList
+//
+//  Created by Lucas Pedrazoli on 21/06/21.
+//
+
+import Foundation
+
+struct Request {
+
+  var endpoint: Endpoint
+  var method: HTTPVerb
+  var params: Array<URLQueryItem> = []
+  var body: [String: Any] = [:]
+  var header: [String: String] = [:]
+
+  init(endpoint: Endpoint, method: HTTPVerb) {
+    self.endpoint = endpoint
+    self.method = method
+  }
+
+  mutating func addQueryItem(_ item: URLQueryItem) {
+    params.append(item)
+  }
+
+  mutating func addHeader(_ name: String, _ value: String) {
+    header[name] = value
+  }
+
+  func build() -> URLRequest {
+    var urlComponents = URLComponents(url: endpoint.rawValue,
+                                            resolvingAgainstBaseURL: false)!
+    urlComponents.queryItems = params
+    var urlRequest = URLRequest(url: urlComponents.url!)
+    urlRequest.httpMethod = method.rawValue
+    return urlRequest
+  }
+
+  mutating func addDefaultParams() {
+    let format = URLQueryItem(name: "format", value: "json")
+    addQueryItem(format)
+  }
+}
