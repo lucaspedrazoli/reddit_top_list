3baf35dc1186d8054eb36058a20e45baf48349d2
Lucas <lucaspedrazoli@gmail.com>
2021-06-21 15:39:26 -0300

    create HTTP verb

---
+++ RedditTopList/Data/Network/HTTPVerb.swift
@@ -0,0 +1,12 @@
+//
+//  HTTPVerb.swift
+//  RedditTopList
+//
+//  Created by Lucas Pedrazoli on 21/06/21.
+//
+
+import Foundation
+
+enum HTTPVerb: String {
+  case GET, POST, PATCH, PUT, DELETE
+}
---
+++ RedditTopList/Data/Network/NetworkDataSource.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkDataSource.swift
+//  RedditTopList
+//
+//  Created by Lucas Pedrazoli on 21/06/21.
+//
+
+import Foundation
