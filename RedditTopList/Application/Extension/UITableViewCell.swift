73cddc584be78725b8d216ce3b503862a1b787bf
Lucas <lucaspedrazoli@gmail.com>
2021-06-22 11:10:44 -0300

    create extension for table view cell

---
+++ RedditTopList/Application/Extension/UITableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  UITableViewCell.swift
+//  RedditTopList
+//
+//  Created by Lucas Pedrazoli on 22/06/21.
+//
+
+import UIKit
+
+extension UITableViewCell {
+
+  static var identifier: String {
+    return String(describing: self)
+  }
+}
