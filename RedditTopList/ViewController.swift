12acefef9070cf879ed216cdea24cce13033ba24
Lucas <lucaspedrazoli@gmail.com>
2021-06-22 09:15:30 -0300

    remove sample code

--- RedditTopList/ViewController.swift
+++ RedditTopList/ViewController.swift
@@ -12,21 +12,6 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .yellow
-   // let storage = UserSessionDataSource()
-//    storage.write("foo", for: "bar")
-//    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) {
-//      let value: String? = storage.read(for: "bar")
-//      print(value ?? "error")
-//    }
-  }
-
-  private func callApi() {
-//    var request = Request(endpoint: .topList, method: .GET)
-//    request.addHeader("Authorization", "Bearer ")
-//    let network = NetworkDataSource()
-//    network.doRequest(request, completion: { (value: TopList?) in
-//      print(value?.data.children.first?.data.title ?? "error")
-//    })
   }
 }
 
