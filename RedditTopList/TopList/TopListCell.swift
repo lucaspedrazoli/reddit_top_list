8127828ffe1f66846ae73a847e7b31d4bead7fae
Lucas <lucaspedrazoli@gmail.com>
2021-06-22 11:15:17 -0300

    create top list cell

---
+++ RedditTopList/TopList/TopListCell.swift
@@ -0,0 +1,55 @@
+//
+//  TopListCell.swift
+//  RedditTopList
+//
+//  Created by Lucas Pedrazoli on 22/06/21.
+//
+
+import Foundation
+import UIKit
+
+class PhotoListCell: UITableViewCell {
+
+  lazy var container: UIView = {
+    let view = UIView()
+    view.translatesAutoresizingMaskIntoConstraints = false
+    view.layer.borderWidth = 2.0
+    view.layer.borderColor = UIColor.black.cgColor
+    view.layer.cornerRadius = 8.0
+    return view
+  }()
+
+  lazy var titleLabel: UILabel = {
+    let label = UILabel()
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }()
+
+  func inflate(with text: String) {
+    titleLabel.text = text
+    addSubviews()
+    installConstraints()
+  }
+
+  private func addSubviews() {
+    contentView.addSubview(container)
+    container.addSubview(titleLabel)
+  }
+
+  private func installConstraints() {
+    let margin = contentView.layoutMarginsGuide
+    var constraints = [
+      container.topAnchor.constraint(equalTo: margin.topAnchor),
+      container.bottomAnchor.constraint(equalTo: margin.bottomAnchor),
+      container.leadingAnchor.constraint(equalTo: margin.leadingAnchor),
+      container.trailingAnchor.constraint(equalTo: margin.trailingAnchor),
+    ]
+    constraints += [
+      titleLabel.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: 10),
+      titleLabel.widthAnchor.constraint(equalTo: container.widthAnchor, multiplier: 0.8),
+      titleLabel.topAnchor.constraint(equalTo: container.topAnchor, constant: 10),
+      titleLabel.bottomAnchor.constraint(equalTo: container.bottomAnchor, constant: -10),
+    ]
+    NSLayoutConstraint.activate(constraints)
+  }
+}
