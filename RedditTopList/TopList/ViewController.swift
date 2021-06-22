bb592fd1a64fe816e8560be9a403fc5d313f9fc1
Lucas <lucaspedrazoli@gmail.com>
2021-06-22 11:24:47 -0300

    wip: render table view cell title

--- RedditTopList/TopList/ViewController.swift
+++ RedditTopList/TopList/ViewController.swift
@@ -7,16 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
   var viewModel: TopListViewModel!
   var topListView: TopListView!
+  var items: [TopListElement] = []
 
   override func viewDidLoad() {
     super.viewDidLoad()
     setupUI()
+  }
+
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return items.count
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return UITableViewCell()
+  }
+
+  private func loadData() {
     viewModel.load {
-      print($0.first?.title ?? "")
+      self.items = $0
+      self.topListView.tableView.reloadData()
     }
   }
 
