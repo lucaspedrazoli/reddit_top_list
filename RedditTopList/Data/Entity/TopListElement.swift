2087fa71e0c9f36b2b90054877dd99b41934706c
Lucas <lucaspedrazoli@gmail.com>
2021-06-23 11:55:49 -0300

    add coding keys and read mutable var

--- RedditTopList/Data/Entity/TopListElement.swift
+++ RedditTopList/Data/Entity/TopListElement.swift
@@ -11,11 +11,26 @@ struct TopListElement: Codable {
   let title: String
   let author: String
   let thumbnail: String
-  let created: Double
-  let created_utc: Double
-  let num_comments: Int
-  let clicked: Bool
-  let visited: Bool
+  let createdAt: Double
+  let commentsCount: Int
   let url: String
+  var read: Bool
+
+  enum CodingKeys: String, CodingKey {
+    case title, author, thumbnail, url
+    case commentsCount = "num_comments"
+    case createdAt = "created_utc"
+  }
+
+  init(from decoder: Decoder) throws {
+    let data = try decoder.container(keyedBy: CodingKeys.self)
+    title = try data.decode(String.self, forKey: .title)
+    author = try data.decode(String.self, forKey: .author)
+    thumbnail = try data.decode(String.self, forKey: .thumbnail)
+    createdAt = try data.decode(Double.self, forKey: .createdAt)
+    commentsCount = try data.decode(Int.self, forKey: .commentsCount)
+    url = try data.decode(String.self, forKey: .url)
+    read = false
+  }
 }
 
