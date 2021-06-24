eef0820e7dfb24e5c093a92cb2a7ebf4a34419ac
Lucas <lucaspedrazoli@gmail.com>
2021-06-24 12:40:21 -0300

    render all items in detail

--- RedditTopList/TopList/DetailViewController.swift
+++ RedditTopList/TopList/DetailViewController.swift
@@ -12,24 +12,134 @@ class DetailViewController: NiblessViewController, SplitControllerlegate {
 
   var item: TopListElement?
 
+  lazy var container: UIView = {
+    let view = UIView()
+    view.translatesAutoresizingMaskIntoConstraints = false
+    view.layer.borderWidth = 2.0
+    view.layer.borderColor = UIColor.black.cgColor
+    view.layer.cornerRadius = 8.0
+    view.backgroundColor = .gray
+    return view
+  }()
+
   lazy var titleLabel: UILabel = {
-    let label = UILabel(frame: CGRect(x: 300, y: 200, width: 200, height: 200))
-    label.text = "aaassasasasggfsdgfgd"
+    let label = UILabel()
+    label.font = UIFont.systemFont(ofSize: 30, weight: .bold)
     label.numberOfLines = 0
     label.textAlignment = .center
-    //label.translatesAutoresizingMaskIntoConstraints = false
+    label.translatesAutoresizingMaskIntoConstraints = false
     return label
+  }()
+
+  lazy var authorLabel: UILabel = {
+    let label = UILabel()
+    label.font = UIFont.systemFont(ofSize: 30, weight: .bold)
+    label.numberOfLines = 0
+    label.textAlignment = .center
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }()
+
+  lazy var entryDateLabel: UILabel = {
+    let label = UILabel()
+    label.font = UIFont.systemFont(ofSize: 30)
+    label.numberOfLines = 0
+    label.textAlignment = .center
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }()
+
+  lazy var commentsLabel: UILabel = {
+    let label = UILabel()
+    label.font = UIFont.systemFont(ofSize: 30)
+    label.textAlignment = .center
+    label.numberOfLines = 0
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }()
+
+  lazy var statusLabel: UILabel = {
+    let label = UILabel()
+    label.font = UIFont.systemFont(ofSize: 30)
+    label.textAlignment = .center
+    label.numberOfLines = 0
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }()
+
+  lazy var thumbnail: UIImageView = {
+    let imageView = UIImageView()
+    imageView.layer.cornerRadius = 16.0
+    imageView.clipsToBounds = true
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    return imageView
   }()
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.addSubview(titleLabel)
-    view.backgroundColor = .yellow
+    addSubviews()
+    installConstraints()
   }
 
   func showDetailItem(_ item: TopListElement?) {
     guard let item = item else { return }
-    titleLabel.text = item.title
+    authorLabel.text = "author: \(item.author)"
+    entryDateLabel.text = Date.elapsedTime(timestamp: item.createdAt)
+    thumbnail.load(url: item.thumbnail)
+    commentsLabel.text = "comments: \(item.commentsCount)"
+    statusLabel.text = "status: \(item.status())"
+    titleLabel.text = "title: \(item.title)"
+  }
+
+  private func addSubviews() {
+    view.addSubview(container)
+    container.addSubview(authorLabel)
+    container.addSubview(entryDateLabel)
+    container.addSubview(thumbnail)
+    container.addSubview(commentsLabel)
+    container.addSubview(statusLabel)
+    container.addSubview(titleLabel)
+  }
+
+  private func installConstraints() {
+    let margin = view.layoutMarginsGuide
+    var constraints = [
+      container.topAnchor.constraint(equalTo: margin.topAnchor),
+      container.bottomAnchor.constraint(equalTo: margin.bottomAnchor),
+      container.leadingAnchor.constraint(equalTo: margin.leadingAnchor),
+      container.trailingAnchor.constraint(equalTo: margin.trailingAnchor),
+    ]
+    constraints += [
+      authorLabel.widthAnchor.constraint(equalTo: container.widthAnchor, multiplier: 0.8),
+      authorLabel.centerXAnchor.constraint(equalTo: container.centerXAnchor),
+      authorLabel.topAnchor.constraint(equalTo: container.topAnchor, constant: 10),
+      authorLabel.bottomAnchor.constraint(equalTo: entryDateLabel.topAnchor),
+    ]
+    constraints += [
+      entryDateLabel.centerXAnchor.constraint(equalTo: container.centerXAnchor),
+      entryDateLabel.bottomAnchor.constraint(equalTo: thumbnail.topAnchor, constant: -10)
+    ]
+    constraints += [
+      thumbnail.centerXAnchor.constraint(equalTo: container.centerXAnchor),
+      thumbnail.widthAnchor.constraint(equalTo: container.widthAnchor, multiplier: 0.25),
+      thumbnail.heightAnchor.constraint(equalTo: container.widthAnchor, multiplier: 0.25),
+      thumbnail.bottomAnchor.constraint(equalTo: commentsLabel.topAnchor, constant: -10)
+    ]
+    constraints += [
+      commentsLabel.centerXAnchor.constraint(equalTo: container.centerXAnchor),
+      commentsLabel.bottomAnchor.constraint(equalTo: statusLabel.topAnchor)
+    ]
+    constraints += [
+      statusLabel.centerXAnchor.constraint(equalTo: container.centerXAnchor),
+      statusLabel.bottomAnchor.constraint(equalTo: titleLabel.topAnchor)
+    ]
+    constraints += [
+      titleLabel.widthAnchor.constraint(equalTo: container.widthAnchor, multiplier: 0.8),
+      titleLabel.centerXAnchor.constraint(equalTo: container.centerXAnchor),
+      titleLabel.topAnchor.constraint(equalTo: statusLabel.bottomAnchor, constant: 10),
+      titleLabel.heightAnchor.constraint(equalToConstant: 50)
+    ]
+    NSLayoutConstraint.activate(constraints)
   }
 
 }
